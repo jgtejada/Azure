dc4580bb5bef50256b9ced04abccad61b0567c37
jgtejada <39434286+jgtejada@users.noreply.github.com>
2018-05-19 13:22:56 +0200

    Delete Jobs.ps1

--- Jobs.ps1
+++
@@ -1,8 +0,0 @@
-﻿$AAN = "Automatizacion-VMs-PRO"
-$RN = "AUTO_START_VM_NameValue"
-$RG = "GR_WEBSERVICES_SP"
-
-$job = (Get-AzureRmAutomationJob –AutomationAccountName $AAN –RunbookName $RN -ResourceGroupName $RG | sort LastModifiedDate –desc)[0]
-$job.Status
-$job.JobParameters
-Get-AzureRmAutomationJobOutput -ResourceGroupName $RG –AutomationAccountName $AAN -Id $job.JobId –Stream Output > C:\Users\javier.gonzalez\Desktop\AAPro.csv

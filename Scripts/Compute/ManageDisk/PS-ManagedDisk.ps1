978a560da68d69326e7e729908a4901d14e9b831
jgtejada <39434286+jgtejada@users.noreply.github.com>
2018-07-06 12:26:40 +0200

    Add files via upload

---
+++ Scripts/Compute/ManageDisk/PS-ManagedDisk.ps1
@@ -0,0 +1,17 @@
+﻿# Logarse en la suscripción desde una ventana de PowerShell
+Login-AzureRmAccount
+
+# Comprobar suscripciones del usuario
+Get-AzureRmSubscription
+
+# Seleccionar subscripción
+Get-AzureRmSubscription –SubscriptionName “Pase para Azure” | Select-AzureRmSubscription
+
+# Crear Grupo de recursos (si no existe) 
+New-AzureRmResourceGroup -Name TestRG -Location westeurope
+
+# Almacenar configuración de disco en variable
+$diskConfig = New-AzureRmDiskConfig -SkuName ‘Standard_LRS‘ -Location westeurope -CreateOption Empty -DiskSizeGB 1
+
+# Creamos el disco administrado
+New-AzureRmDisk -DiskName psmd -Disk $diskConfig -ResourceGroupName TestRG

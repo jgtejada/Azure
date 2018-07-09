5b6a2f3f6d1831b5a59bc7df9e66b4dffc6c9d82
jgtejada <39434286+jgtejada@users.noreply.github.com>
2018-07-09 10:11:18 +0200

    Add files via upload

---
+++ Scripts/Compute/Tags/PS-Tags.ps1
@@ -0,0 +1,15 @@
+﻿# Logarse en la suscripción desde una ventana de PowerShell
+Login-AzureRmAccount
+
+# Comprobar suscripciones del usuario
+Get-AzureRmSubscription
+
+# Seleccionar subscripción
+Get-AzureRmSubscription –SubscriptionName “Pase para Azure” | Select-AzureRmSubscription
+
+# Crear Grupo de recursos 
+New-AzureRmResourceGroup -Name TestRG -Location westeurope
+
+# Asignar tags a VM
+$tags = @{Propietario="SoftwareONE";Centro_Coste="Desarrollo"}
+Set-AzureRmResource -ResourceGroupName $ResourceGroupName -Name $VMNameRT -ResourceType "Microsoft.Compute/VirtualMachines" -Tag $tags -Force

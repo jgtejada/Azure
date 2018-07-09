437b48eb0513ec28444f8ee949bc6cede2ab542e
jgtejada <39434286+jgtejada@users.noreply.github.com>
2018-07-09 10:12:04 +0200

    Add files via upload

---
+++ Scripts/Security/LockResource/PS-Lock.ps1
@@ -0,0 +1,15 @@
+﻿# Logarse en la suscripción desde una ventana de PowerShell
+Login-AzureRmAccount
+
+# Comprobar suscripciones del usuario
+Get-AzureRmSubscription
+
+# Seleccionar subscripción
+Get-AzureRmSubscription –SubscriptionName “Pase para Azure” | Select-AzureRmSubscription
+
+# Añadir bloqueo "CanNotDelete" a la VM
+New-AzureRmResourceLock -LockLevel CanNotDelete -LockName LockVMPS -ResourceName (VMName) -ResourceType Microsoft.Compute/virtualMachines -ResourceGroupName (GRName)
+
+# Eliminar el blqoueo
+$lockId = (Get-AzureRmResourceLock -ResourceGroupName GR-Name -ResourceName VMName -ResourceType Microsoft.Compute/virtualMachines).LockId
+Remove-AzureRmResourceLock -LockId $lockId
